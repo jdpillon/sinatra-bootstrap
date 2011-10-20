8f9787339d93ccc8a83f80fc30a09e0728566c46
Jacques-Daniel PILLON <jdpillon@lesalternatives.org>
2011-10-20 09:31:24 +0200

    Jquery update, jquery ui, controllers

--- app.rb
+++ app.rb
@@ -2,15 +2,20 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 
-# Helpers
-require './lib/render_partial'
 
 # Set Sinatra variables
 set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
 set :views, 'views'
-set :public, 'public'
+set :public_folder, 'public'
 set :haml, {:format => :html5} # default Haml format is :xhtml
+
+
+# Helpers
+require './lib/render_partial'
+
+# Controllers
+Dir[settings.root + "controllers/**/*.rb"].each{|f| require f}
 
 # Application routes
 get '/' do
